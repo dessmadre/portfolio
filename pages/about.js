0f0d279fd22599a65c60e9733cab50f385db5cc2
Jose Duarte <joseduarte@joses-air.home>
2021-03-29 19:10:33 -0700

    yo

--- pages/about.js
+++ pages/about.js
@@ -2,23 +2,24 @@ import Container from 'components/Container';
 import Meta from 'components/Meta';
 
 const About = () => {
-	return (
-		<>
-			<Meta />
-			<Container>
-				<div className='flex items-center justify-center flex-wrap flex-col w-full h-full'>
-					<h1
-						className='
-					py-24 text-5xl font-bold'>
-						About Me<span className='text-green-400'>.</span>
-					</h1>
-					<p className='text-2xl font-semibold w-3/5'>
-						My name is Jose Duarte, I am a Los Angeles based web-developer.
-					</p>
-				</div>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Meta />
+      <Container>
+        <div className='flex items-center justify-center flex-wrap flex-col w-full h-full'>
+          <h1
+            className='
+					py-24 text-5xl font-bold'
+          >
+            About Me<span className='text-green-400'>.</span>
+          </h1>
+          <p className='text-2xl font-semibold w-3/5'>
+            My name is Jose Duarte, I am not Los Angeles based web-developer.
+          </p>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default About;
